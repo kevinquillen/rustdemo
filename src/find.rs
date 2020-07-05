8f39cfab7a99e57ccf1b6af3d9a1773dd2623a14
Kevin Quillen <kevinquillen@gmail.com>
2020-07-05 14:22:54 -0400

    Getting slimmer and more organized, but is this the best Rust can do? Make the code previously in main() testable.

---
+++ src/find.rs
@@ -0,0 +1,87 @@
+use regex::Regex;
+use std::io::{prelude::*, BufReader};
+use std::fs::File;
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+pub struct FindText {
+    #[structopt(
+        short = "p",
+        long = "pattern",
+        help = "The text you want to look for.",
+    )]
+    pub pattern: String,
+
+    #[structopt(
+        short = "f",
+        long = "filepath",
+        help = "The directory to search for the pattern given.",
+    )]
+    pub path: PathBuf
+}
+
+pub fn find_string(content: String, pattern: &str) -> bool {
+    return Regex::new(format!(r#"(?i){}"#, pattern).as_str()).unwrap().is_match(&content.as_str());
+}
+
+pub fn find_matches(pattern: &String, path: &PathBuf) -> Vec<i32> {
+    let file = File::open(path).expect("\n\nCould not read from file!");
+    let reader = BufReader::new(file);
+    let mut hits = Vec::new();
+    let mut line_number = 1;
+
+    for line in reader.lines() {
+        if find_string(line.unwrap(), pattern) {
+            hits.push(line_number);
+        }
+
+        line_number = line_number + 1;
+    }
+
+    hits
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_string() {
+        let content = String::from("Hello there.");
+        let pattern = "there";
+        assert_eq!(true, find_string(content, pattern));
+
+        let content = String::from("Goodbye!");
+        let pattern = "GOOD";
+        assert_eq!(true, find_string(content, pattern));
+
+        let content = String::from("I am a ParTiAL mixed case");
+        let pattern = "part";
+        assert_eq!(true, find_string(content, pattern));
+
+        let content = String::from("good bye");
+        let pattern = "hello";
+        assert_eq!(false, find_string(content, pattern));
+
+        let content = String::from("hel lo");
+        let pattern = "hello";
+        assert_eq!(false, find_string(content, pattern));
+    }
+
+    #[test]
+    fn test_find_matches() {
+        let pattern = String::from("Hello there.");
+        let path = PathBuf::from("fixtures/example.txt");
+        let matches = find_matches(&pattern, &path);
+        assert_eq!(1, matches.len());
+        assert_eq!(true, matches.contains(&1));
+
+        let pattern = String::from("dol");
+        let path = PathBuf::from("fixtures/example.txt");
+        let matches = find_matches(&pattern, &path);
+        assert_eq!(2, matches.len());
+        assert_eq!(true, matches.contains(&3));
+        assert_eq!(true, matches.contains(&7));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,13 @@
 use structopt::StructOpt;
-use std::io::{prelude::*, BufReader};
-use std::fs::File;
-use std::path::PathBuf;
 
-mod find_match;
-
-#[derive(StructOpt)]
-struct CommandOptions {
-    #[structopt(
-        short = "p", 
-        long = "pattern",
-        help = "The text you want to look for.",
-    )]
-    pattern: String,
-
-    #[structopt(
-        short = "f",
-        long = "filepath",
-        help = "The directory to search for the pattern given.",
-    )]
-    path: PathBuf
-}
+mod find;
 
 fn main() {
-    let args = CommandOptions::from_args();
-    let file = File::open(&args.path).expect("\n\nCould not read from file!");
-    let reader = BufReader::new(file);
-    let mut hits = Vec::new();
-    let mut line_number = 1;
-
-    for line in reader.lines() {
-        if find_match::find_match(line.unwrap(), &args.pattern) {
-            hits.push(line_number);
-        }
-        
-        line_number = line_number + 1;
-    }
+    let command_args = find::FindText::from_args();
+    let hits = find::find_matches(&command_args.pattern, &command_args.path);
 
     match hits.len() > 0 {
-        false => println!("Text \"{}\" not found in {:?}.", args.pattern, args.path),
+        false => println!("Text \"{}\" not found in {:?}.", command_args.pattern, command_args.path),
         true => {
             println!("\nFound {} matches: ", hits.len());
 

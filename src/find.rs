d7720586170e0d5f26b7a9df6bc7e8ffd6170c30
Kevin Quillen <kevinquillen@gmail.com>
2020-07-12 08:43:35 -0400

    Following the Book with structs with methods - is this design 'better'?

--- src/find.rs
+++ src/find.rs
@@ -11,7 +11,7 @@ pub struct FindText {
         long = "pattern",
         help = "The text you want to look for.",
     )]
-    pub pattern: String,
+    pub text: String,
 
     #[structopt(
         short = "f",
@@ -21,38 +21,40 @@ pub struct FindText {
     pub path: PathBuf
 }
 
-fn find_string(content: String, pattern: &str) -> bool {
-    return Regex::new(format!(r#"(?i){}"#, pattern).as_str()).unwrap().is_match(&content.as_str());
-}
-
-pub fn find_matches(pattern: &String, path: &PathBuf) -> Vec<i32> {
-    let file = File::open(path).expect("\n\nCould not read from file!");
-    let reader = BufReader::new(file);
-    let mut matches = Vec::new();
-    let mut line_number = 1;
-
-    for line in reader.lines() {
-        if find_string(line.unwrap(), pattern) {
-            matches.push(line_number);
-        }
-
-        line_number = line_number + 1;
+impl FindText {
+    fn find_string(&self, content: String) -> bool {
+        Regex::new(format!(r#"(?i){}"#, self.text).as_str()).unwrap().is_match(content.as_str())
     }
 
-    matches
-}
+    pub fn find_matches(&self) -> Vec<i32> {
+        let file = File::open(self.path.to_str().unwrap()).expect("\n\nCould not read from file!");
+        let reader = BufReader::new(file);
+        let mut matches = Vec::new();
+        let mut line_number = 1;
 
-pub fn output_matches(matches: Vec<i32>, pattern: &String, path: &PathBuf) {
-    match matches.len() > 0 {
-        false => println!("Text \"{}\" not found in {:?}.", pattern, path),
-        true => {
-            println!("\nFound {} matches: ", matches.len());
-
-            for line in &matches {
-                println!(" - Found a match on line #{}.", line)
+        for line in reader.lines() {
+            if self.find_string(line.unwrap()) {
+                matches.push(line_number);
             }
-        },
-    };
+
+            line_number = line_number + 1;
+        }
+
+        matches
+    }
+
+    pub fn output_matches(&self, matches: Vec<i32>) {
+        match matches.len() > 0 {
+            false => println!("No text was found."),
+            true => {
+                println!("\nFound {} matches: ", matches.len());
+
+                for line in &matches {
+                    println!(" - Found a match on line #{}.", line)
+                }
+            },
+        };
+    }
 }
 
 #[cfg(test)]
@@ -61,38 +63,35 @@ mod tests {
 
     #[test]
     fn test_find_string() {
-        let content = String::from("Hello there.");
-        let pattern = "there";
-        assert_eq!(true, find_string(content, pattern));
+        let fixture = "fixtures/example.txt";
 
-        let content = String::from("Goodbye!");
-        let pattern = "GOOD";
-        assert_eq!(true, find_string(content, pattern));
+        let op = FindText { text: String::from("there"), path: PathBuf::from(fixture) };
+        assert_eq!(true, op.find_string(String::from("Hello there")));
 
-        let content = String::from("I am a ParTiAL mixed case");
-        let pattern = "part";
-        assert_eq!(true, find_string(content, pattern));
+        let op = FindText { text: String::from("GOOD"), path: PathBuf::from(fixture) };
+        assert_eq!(true, op.find_string(String::from("Goodbye!")));
 
-        let content = String::from("good bye");
-        let pattern = "hello";
-        assert_eq!(false, find_string(content, pattern));
+        let op = FindText { text: String::from("part"), path: PathBuf::from(fixture) };
+        assert_eq!(true, op.find_string(String::from("I am a ParTiAL mixed case")));
 
-        let content = String::from("hel lo");
-        let pattern = "hello";
-        assert_eq!(false, find_string(content, pattern));
+        let op = FindText { text: String::from("good bye"), path: PathBuf::from(fixture) };
+        assert_eq!(false, op.find_string(String::from("hello")));
+
+        let op = FindText { text: String::from("hel lo"), path: PathBuf::from(fixture) };
+        assert_eq!(false, op.find_string(String::from("hello")));
     }
 
     #[test]
     fn test_find_matches() {
-        let pattern = String::from("Hello there.");
-        let path = PathBuf::from("fixtures/example.txt");
-        let matches = find_matches(&pattern, &path);
+        let fixture = "fixtures/example.txt";
+
+        let op = FindText { text: String::from("Hello there!"), path: PathBuf::from(fixture) };
+        let matches = op.find_matches();
         assert_eq!(1, matches.len());
         assert_eq!(true, matches.contains(&1));
 
-        let pattern = String::from("dol");
-        let path = PathBuf::from("fixtures/example.txt");
-        let matches = find_matches(&pattern, &path);
+        let op = FindText { text: String::from("dol"), path: PathBuf::from(fixture) };
+        let matches = op.find_matches();
         assert_eq!(2, matches.len());
         assert_eq!(true, matches.contains(&3));
         assert_eq!(true, matches.contains(&7));
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,10 @@
 use structopt::StructOpt;
 
 mod find;
+use find::FindText;
 
 fn main() {
-    let command_args = find::FindText::from_args();
-    let matches = find::find_matches(&command_args.pattern, &command_args.path);
-    find::output_matches(matches, &command_args.pattern, &command_args.path);
+    let op = FindText::from_args();
+    let matches = op.find_matches();
+    op.output_matches(matches);
 }

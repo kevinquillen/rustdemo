4122da4235645224419f1aa8eb754ad04a4d0044
Kevin Quillen <kevinquillen@gmail.com>
2020-08-15 23:07:14 -0400

    Reduce complexity as we follow along with the book. Having the one method return a bool on match just to unit test it does not provide a lot of benefit in this context when we can just do that from the test_find_matches test without losing any current functionality.

---
+++ src/lib.rs
@@ -0,0 +1,75 @@
+use regex::Regex;
+use std::io::{prelude::*, BufReader};
+use std::fs::File;
+use structopt::StructOpt;
+use std::path::PathBuf;
+
+#[derive(StructOpt)]
+pub struct Find {
+    #[structopt(
+        short = "t",
+        long = "text",
+        help = "The text you want to look for.",
+    )]
+    pub text: String,
+
+    #[structopt(
+        short = "f",
+        long = "file",
+        help = "The file to search for the given text.",
+    )]
+    pub path: PathBuf
+}
+
+impl Find {
+    pub fn find_matches(&self) -> Vec<i32> {
+        let file = File::open(self.path.to_str().unwrap()).expect("\n\nCould not read from file!");
+        let reader = BufReader::new(file);
+        let re = Regex::new(format!(r#"(?i){}"#, self.text).as_str()).unwrap();
+        let mut matches = Vec::new();
+        let mut line_number = 1;
+
+        for line in reader.lines() {
+            if re.is_match(line.unwrap().as_str()) {
+                matches.push(line_number);
+            }
+
+            line_number = line_number + 1;
+        }
+
+        matches
+    }
+
+    pub fn list_matches(&self, matches: Vec<i32>) {
+        if matches.is_empty() {
+            println!("No matches found.");
+        } else {
+            println!("\nFound {} matches: ", matches.len());
+
+            for line in &matches {
+                println!(" - Found a match on line #{}.", line)
+            }
+        }
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_find_matches() {
+        let fixture = "fixtures/example.txt";
+
+        let find = Find { text: String::from("Hello there!"), path: PathBuf::from(fixture) };
+        let matches = find.find_matches();
+        assert_eq!(1, matches.len());
+        assert_eq!(true, matches.contains(&1));
+
+        let find = Find { text: String::from("dol"), path: PathBuf::from(fixture) };
+        let matches = find.find_matches();
+        assert_eq!(2, matches.len());
+        assert_eq!(true, matches.contains(&3));
+        assert_eq!(true, matches.contains(&7));
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,8 @@
+use rustdemo::Find;
 use structopt::StructOpt;
 
-mod find;
-use find::FindText;
-
 fn main() {
-    let op = FindText::from_args();
-    let matches = op.find_matches();
-    op.output_matches(matches);
+    let find = Find::from_args();
+    let matches = find.find_matches();
+    find.list_matches(matches);
 }
